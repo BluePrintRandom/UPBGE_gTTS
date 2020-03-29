5aa9b6254b7bd212cc25e594c81403552541910d
Jacob Merrill <BluePrintRandom1@gmail.com>
2020-03-29 15:55:39 -0700

    Update upbge_gTTS.py

--- upbge_gTTS.py
+++ upbge_gTTS.py
@@ -2,7 +2,7 @@
 Copyright <2020> <Jacob Merrll / BluePrintRandom1@gmail.com>
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
-"Be cool man"
+
 The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
@@ -29,7 +29,11 @@ if bge.logic.getCurrentController().sensors['Always'].positive or bge.logic.getC
             
         
     tts.save('hello.mp3')
-    device = aud.Device()
+    if 'device' not in own:
+        device = aud.Device()
+        own['device']=device
+    else:
+        device =   own['device']  
     factory = aud.Sound.file('hello.mp3')
     handle = device.play(factory)
     handle.pitch =1.25
